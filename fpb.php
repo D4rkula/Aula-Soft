b04cf0caefee2745af373dea6bb711269252414f
Giovi Simmons <116762213+D4rkula@users.noreply.github.com>
2023-05-02 10:52:43 -0300

    Add files via upload

---
+++ fpb.php
@@ -0,0 +1,11 @@
+<?php
+
+$valor = 5;
+$fpb = "FPB";
+
+for ($i = 0; $i <= $valor; $i++) {
+    echo $fpb."<br>";
+    $fpb .= "FPB";
+}
+
+?>
---
+++ valor.php
@@ -0,0 +1,10 @@
+<?php
+
+$valor = 5;
+$soma = 0;
+for ($i = 0; $i <= $valor; $i++) {
+    $soma += $i;
+}
+echo $soma;
+
+?>
