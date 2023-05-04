6ca121e35a1be421595ebf0f5f9a778b2f64dbf1
Giovi Simmons <116762213+D4rkula@users.noreply.github.com>
2023-05-04 11:13:46 -0300

    Update parcelas.php

--- parcelas.php
+++ parcelas.php
@@ -2,6 +2,7 @@
 
 $v = 1200;
 
-echo ($v / 12)."<br>".($v / 12) + ($v / 100 * 12)
+//valor parcelado em 12 + 1% do valor  //  valor total com os juros
+echo ($v / 12) + ($v / 100)."<br>".(($v / 12) + ($v / 100)) * 12
 
 ?>
